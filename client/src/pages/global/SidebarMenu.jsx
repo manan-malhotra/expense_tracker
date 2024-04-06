4aa281979637b679ae2eafb23ce455c604eae02c
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-04-06 23:49:33 +0530

    Minor ui changes

--- client/src/pages/global/SidebarMenu.jsx
+++ client/src/pages/global/SidebarMenu.jsx
@@ -1,133 +1,162 @@
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
-import { sidebarMenu } from '../../constants/sidebarMenu';
+import { sidebarMenu } from "../../constants/sidebarMenu";
 import { useLocation } from "react-router-dom";
-import { tokens } from "../../styles/theme";
-import { useState } from "react";
+import { ColorModeContext, tokens } from "../../styles/theme";
+import { useContext, useState } from "react";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import SidebarMenuItem from "./SidebarMenuItem";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import LogoutIcon from "@mui/icons-material/Logout";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import "react-pro-sidebar/dist/css/styles.css";
-
-
 const SidebarMenu = () => {
-
-  const location = useLocation();
-  const urlPathName = location.pathname;
-
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const [selected, setSelected] = useState(urlPathName);
-  const [isCollapsed, setIsCollapsed] = useState(false);
-
-  return (
-    <Box
-      sx={{
-        "& .pro-sidebar-inner": {
-          background: `${colors.primary[400]} !important`,
-        },
-        "& .pro-icon-wrapper": {
-          backgroundColor: "transparent !important",
-        },
-        "& .pro-inner-item": {
-          padding: "5px 35px 5px 20px !important",
-        },
-        "& .pro-inner-item:hover": {
-          color: "#868dfb !important",
-        },
-        "& .pro-menu-item.active": {
-          color: "#6870fa !important",
-        },
-      }}
-    >
-      <ProSidebar collapsed={isCollapsed}>
-
-        <Menu iconShape="square">
-
-          {/* LOGO AND MENU ICON */}
-          <MenuItem
-            onClick={() => setIsCollapsed(!isCollapsed)}
-            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
-            style={{
-              margin: "10px 0 20px 0",
-              color: colors.grey[100],
-            }}
-          >
-            {
-              !isCollapsed && (
+    const colorMode = useContext(ColorModeContext);
+    const location = useLocation();
+    const urlPathName = location.pathname;
+    const [id, setId] = useState(0);
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [selected, setSelected] = useState(urlPathName);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    if (selected !== "/home")
+        return (
+            <>
                 <Box
-                  display="flex"
-                  justifyContent="space-between"
-                  alignItems="center"
-                  ml="15px"
+                    sx={{
+                        "& .pro-sidebar-inner": {
+                            background: `${colors.primary[400]} !important`,
+                        },
+                        "& .pro-icon-wrapper": {
+                            backgroundColor: "transparent !important",
+                        },
+                        "& .pro-inner-item": {
+                            padding: "5px 35px 5px 20px !important",
+                        },
+                        "& .pro-inner-item:hover": {
+                            color: "#868dfb !important",
+                        },
+                        "& .pro-menu-item.active": {
+                            color: "#6870fa !important",
+                        },
+                    }}
                 >
-                  <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
-                    <MenuOutlinedIcon />
-                  </IconButton>
+                    <ProSidebar collapsed={isCollapsed}>
+                        <Menu iconShape="square">
+                            {/* LOGO AND MENU ICON */}
+                            <MenuItem
+                                onClick={() => setIsCollapsed(!isCollapsed)}
+                                icon={
+                                    isCollapsed ? (
+                                        <MenuOutlinedIcon />
+                                    ) : undefined
+                                }
+                                style={{
+                                    margin: "10px 0 20px 0",
+                                    color: colors.grey[100],
+                                }}
+                            >
+                                {!isCollapsed && (
+                                    <Box
+                                        display="flex"
+                                        justifyContent="space-between"
+                                        alignItems="center"
+                                        ml="15px"
+                                    >
+                                        <IconButton
+                                            onClick={() =>
+                                                setIsCollapsed(!isCollapsed)
+                                            }
+                                        >
+                                            <MenuOutlinedIcon />
+                                        </IconButton>
+                                    </Box>
+                                )}
+                            </MenuItem>
+
+                            {!isCollapsed && (
+                                <Box mb="25px">
+                                    <Box
+                                        display="flex"
+                                        justifyContent="center"
+                                        alignItems="center"
+                                    >
+                                        <img
+                                            alt="profile-user"
+                                            width="100px"
+                                            height="100px"
+                                            src={`https://raw.githubusercontent.com/manan-malhotra/heal.in-client/test/assets/avatars/male/${
+                                                id % 7
+                                            }.png`}
+                                            style={{
+                                                cursor: "pointer",
+                                                borderRadius: "50%",
+                                            }}
+                                        />
+                                    </Box>
+
+                                    <Box textAlign="center">
+                                        <Typography
+                                            variant="h2"
+                                            fontWeight="bold"
+                                            color={colors.grey[100]}
+                                            sx={{ m: "10px 0 0 0" }}
+                                        >
+                                            Yash Talele
+                                        </Typography>
+                                    </Box>
+                                </Box>
+                            )}
+
+                            <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+                                {sidebarMenu.map((menu) =>
+                                    menu.tag === "divider" ? (
+                                        <Typography
+                                            variant="h6"
+                                            key={menu.title}
+                                            color={colors.grey[300]}
+                                            sx={{ m: "15px 0 5px 20px" }}
+                                        >
+                                            {menu.title}
+                                        </Typography>
+                                    ) : (
+                                        <SidebarMenuItem
+                                            key={menu.title}
+                                            menu={menu}
+                                            selected={selected}
+                                            setSelected={setSelected}
+                                        />
+                                    )
+                                )}
+                            </Box>
+                        </Menu>
+                        <Box
+                            display="flex"
+                            marginLeft="auto"
+                            marginRight="auto"
+                            marginTop="auto"
+                            marginBottom="5%"
+                        >
+                            <IconButton onClick={colorMode.toggleColorMode}>
+                                {
+                                    // by user click toggle UI theme color...
+                                    theme.palette.mode === "dark" ? (
+                                        <DarkModeOutlinedIcon />
+                                    ) : (
+                                        <LightModeOutlinedIcon />
+                                    )
+                                }
+                            </IconButton>
+
+                            <IconButton>
+                                <LogoutIcon />
+                            </IconButton>
+                        </Box>
+                    </ProSidebar>
                 </Box>
-              )
-            }
-          </MenuItem>
-
-          {/* 🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧 */}
-          {
-            !isCollapsed && (
-              <Box mb="25px">
-
-                <Box display="flex" justifyContent="center" alignItems="center">
-                  <img
-                    alt="profile-user"
-                    width="100px"
-                    height="100px"
-                    src={require('../../assets/0.png')}
-                    style={{ cursor: "pointer", borderRadius: "50%" }}
-                  />
-                </Box>
-
-                <Box textAlign="center">
-                  <Typography
-                    variant="h2"
-                    fontWeight="bold"
-                    color={colors.grey[100]}
-                    sx={{ m: "10px 0 0 0" }}
-                  >
-                    Yash Talele
-                  </Typography>
-                </Box>
-
-              </Box>
-            )
-          }
-
-          {/* 🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧 */}
-          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
-            {
-              sidebarMenu.map(menu =>
-                menu.tag === 'divider'
-                  ?
-                  <Typography
-                    variant="h6"
-                    key={menu.title}
-                    color={colors.grey[300]}
-                    sx={{ m: "15px 0 5px 20px" }}
-                  >
-                    {menu.title}
-                  </Typography>
-                  :
-                  <SidebarMenuItem
-                    key={menu.title}
-                    menu={menu}
-                    selected={selected}
-                    setSelected={setSelected}
-                  />
-              )
-            }
-          </Box>
-
-        </Menu>
-
-      </ProSidebar>
-    </Box>
-  );
+            </>
+        );
 };
 
 export default SidebarMenu;
