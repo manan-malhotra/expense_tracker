9879cf33cf87433162952f7012d5a1d6e73e783a
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-04-10 14:36:07 +0530

    UI Minor bug fix

--- client/src/pages/info/TransactionHistory.jsx
+++ client/src/pages/info/TransactionHistory.jsx
@@ -4,71 +4,71 @@ import { tokens } from "../../styles/theme";
 import { Header } from "../../components";
 import { Box } from "@mui/material";
 import { useGlobalContext } from "../../context/globalContext";
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import transactionHistoryColumns from "../../constants/transactionHistoryColumns";
 
-
 const TransactionHistory = () => {
-  const { transactionHistory } = useGlobalContext();
-  const [history, setHistory] = useState([]);
-  useEffect(() => {
-    transactionHistory().then((history) => {
-      setHistory(history);
-    })
-  }, [])
+    const { transactionHistory } = useGlobalContext();
+    const [history, setHistory] = useState([]);
+    useEffect(() => {
+        transactionHistory().then((history) => {
+            console.log(history);
+            setHistory(history);
+        });
+    }, []);
 
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
 
-  return (
-    <Box m="20px">
-
-      <Header title="Transaction History" subtitle="List of Transactions for Future Reference" />
-      <Box
-        m="40px 0 0 0"
-        height="75vh"
-        // custom css for material ui
-        sx={{
-          "& .MuiDataGrid-root": {
-            border: "none",
-          },
-          "& .MuiDataGrid-cell": {
-            borderBottom: "none",
-          },
-          "& .name-column--cell": {
-            color: colors.greenAccent[300],
-          },
-          "& .MuiDataGrid-columnHeaders": {
-            backgroundColor: colors.blueAccent[700],
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-virtualScroller": {
-            backgroundColor: colors.primary[400],
-          },
-          "& .MuiDataGrid-footerContainer": {
-            borderTop: "none",
-            backgroundColor: colors.blueAccent[700],
-          },
-          "& .MuiCheckbox-root": {
-            color: `${colors.greenAccent[200]} !important`,
-          },
-          // 🟧🟧🟧 data filter tool-bar present here 🟧🟧🟧
-          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-            color: `${colors.grey[100]} !important`,
-          },
-        }}
-      >
-
-        <DataGrid
-        getRowId={(row) => row._id}
-          rows={history}
-          columns={transactionHistoryColumns(colors)}
-          components={{ Toolbar: GridToolbar }}
-        />
-
-      </Box>
-    </Box>
-  );
+    return (
+        <Box m="20px">
+            <Header
+                title="Transaction History"
+                subtitle="List of Transactions for Future Reference"
+            />
+            <Box
+                m="40px 0 0 0"
+                height="75vh"
+                // custom css for material ui
+                sx={{
+                    "& .MuiDataGrid-root": {
+                        border: "none",
+                    },
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none",
+                    },
+                    "& .name-column--cell": {
+                        color: colors.greenAccent[300],
+                    },
+                    "& .MuiDataGrid-columnHeaders": {
+                        backgroundColor: colors.blueAccent[700],
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: colors.primary[400],
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        borderTop: "none",
+                        backgroundColor: colors.blueAccent[700],
+                    },
+                    "& .MuiCheckbox-root": {
+                        color: `${colors.greenAccent[200]} !important`,
+                    },
+                    // 🟧🟧🟧 data filter tool-bar present here 🟧🟧🟧
+                    "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                        color: `${colors.grey[100]} !important`,
+                    },
+                }}
+            >
+                <DataGrid
+                    getRowId={(row) => row._id}
+                    rows={history}
+                    columns={transactionHistoryColumns(colors)}
+                    components={{ Toolbar: GridToolbar }}
+                />
+            </Box>
+        </Box>
+    );
 };
 
 export default TransactionHistory;
