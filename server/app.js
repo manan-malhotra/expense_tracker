48ad004d174da8c991dd1757deb87fbd0b8f988f
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2024-04-10 12:11:48 +0530

    Changed get message

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
-    res.send("I am alive");
+    res.send("Server is up and running");
 });
 app.use("/transactions", require("./routes/transactionsRouter"));
 app.use("/users", require("./routes/userRouter"));
