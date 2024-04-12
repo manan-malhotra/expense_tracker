1b2b3534de0d9cb334ee31597a07a515ddaabe0c
Yash <yashrtalele@gmail.com>
2024-04-12 10:51:19 +0530

    Added prometheus metrics

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 require("dotenv").config();
+const client = require("prom-client");
 const app = require("./app");
 const PORT = process.env.PORT || 3000;
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware.js");
 app.use(errorHandler);
 
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+collectDefaultMetrics({ register: client.register });
+
+app.get("/metrics", async (req, res) => {
+  res.setHeader("Content-Type", client.register.contentType);
+  const metrics = await client.register.metrics();
+  res.send(metrics);
+});
 const server = () => {
-    connectDB();
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+  connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 };
 
 server();
